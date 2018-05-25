5bd601b09ededf4bf3f30d0b26cbb20c7770d082
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-24 21:15:12 -0500

    Sample Git Search

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   }
 
   getUserName= () => {
+    if(document.getElementById('input').value !=='') 
+    {
     const name= this.refs.name.value;
     fetch(`http://api.github.com/users/${name}`)
     .then(response =>response.json()) 
@@ -16,7 +18,8 @@ class App extends Component {
         console.log(data)
       })
     })
-
+      document.getElementById('input').value='';
+    }
   }
 
   render() {
@@ -26,7 +29,7 @@ class App extends Component {
           <h1 className="App-title">Git HUb Search</h1>
         </header>
         <p className="App-intro">Search Git UserName </p>
-        <input type= "text" placeholder="Enter UserName " ref='name'/>
+        <input type= "text" placeholder="Enter UserName " ref='name' id="input"/>
         <button onClick={this.getUserName}>Search user</button>
         <br/>
         <p>{this.state.users}</p>
